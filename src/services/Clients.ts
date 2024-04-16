d7a3808700d33243c21c1961d159d4ac029a8103
s.bustos@want.cl <s.bustos@want.cl>
2024-04-16 19:34:30 -0400

    refactor: get clientes service

---
+++ src/services/Clients.ts
@@ -0,0 +1,65 @@
+export const CLIENTS = [
+  {
+    id: "01",
+    client: "UCC Cargo & Logistic Solutions",
+    description: {
+      es: "Diseño realizado por agencia. Desarrollo a medida de Theme WordPress.",
+      en: "Custom made design and WordPress theme, based on starting theme developed for the agency.",
+    },
+    tags: ["wordpress", "php", "css/sass", "jquery", "javascript", "agency"],
+    image: "/projects/web-ucclog.webp",
+    siteUrl: "https://web.ucclog.com/",
+  },
+  {
+    id: "02",
+    client: "San Pedro Equipos Ltda.",
+    description: {
+      es: "Diseño propio. Desarrollado en HTML5/CSS/JAVASCRIPT/JQUERY con funcionalidades de phpmailer.",
+      en: "Landing page for company, based on HTML5, CSS (SASS as preprocessor), Javascript and PHP for data colleting.",
+    },
+    tags: ["html5", "php", "css/sass", "jquery", "javascript"],
+    image: "/projects/web-sanpedro.webp",
+    siteUrl: "https://sanpedroequipos.cl/",
+  },
+  {
+    id: "03",
+    client: "Quality Inspección y Certificaciones",
+    description: {
+      es: "Diseño propio. Desarrollado en HTML5/CSS/JAVASCRIPT/JQUERY con funcionalidades de phpmailer.",
+      en: "Landing page for company, based on HTML5, CSS (SASS as preprocessor), Javascript and PHP for data colleting.",
+    },
+    tags: ["html5", "php", "css/sass", "jquery", "javascript"],
+    image: "/projects/web-quality.webp",
+    siteUrl: "https://www.qualityic.cl/",
+  },
+  {
+    id: "04",
+    client: "Fundo Río Los Ostiones",
+    description: {
+      es: "Diseño propio. Desarrollado en HTML5/CSS/JAVASCRIPT/JQUERY con funcionalidades de phpmailer.",
+      en: "Landing page for company, based on HTML5, CSS (SASS as preprocessor), Javascript and PHP for data colleting.",
+    },
+    tags: ["html5", "php", "css/sass", "jquery", "javascript"],
+    image: "/projects/web-fundo.webp",
+    siteUrl: "https://www.fundoriolosostiones.cl/",
+  },
+  {
+    id: "05",
+    client: "Teletón",
+    description: {
+      es: "Desarrollo de herramientas de accesibilidad a medida, solicitadas por cliente. Diseño realizado por agencia. Desarrollo a medida de Theme WordPress.",
+      en: "Development of customized accesibility tools, client required. Custom made design and WordPress theme, based on starting theme developed for the agency.",
+    },
+    tags: [
+      "wordpress",
+      "php",
+      "css/sass",
+      "jquery",
+      "javascript",
+      "accesibilidad",
+      "agency",
+    ],
+    image: "/projects/web-teleton.webp",
+    siteUrl: "https://www.teleton.cl/",
+  },
+];
