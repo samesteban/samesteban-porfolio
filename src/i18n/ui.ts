191b60566f82bdd0fa56fe7c0a533424eb411d65
s.bustos@want.cl <s.bustos@want.cl>
2024-04-16 16:58:36 -0400

    feat: add english language

---
+++ src/i18n/ui.ts
@@ -0,0 +1,46 @@
+export const languages = {
+  es: "Es",
+  en: "En",
+};
+
+export const defaultLang = "es";
+
+export const ui = {
+  es: {
+    "tab.title":
+      "Sam Esteban | Diseñador y Desarrollador Full Stack, orientado a Front-End",
+    "nav.home": "Inicio",
+    "nav.portfolio": "Portafolio",
+    "nav.contact": "Contacto",
+    "hero.hi": "Hola, soy",
+    "hero.name": "Sam Esteban",
+    "hero.job":
+      "Diseñador web y Desarrollador Full Stack, orientado a Front-End con +10 años de experiencia.",
+    "hero.years": "Llevo 35 vueltas al astro rey",
+    "hero.desk": "y mi escritorio está en Chile.",
+    "hero.knowledge": "Mi conocimiento está basado en:",
+    "btn.viewSite": "Ver Sitio",
+    "btn.viewPortfolio": "Ver Portafolio",
+    "title.portfolio": "Portafolio",
+    "title.contact": "Conversemos",
+    "footer.copyright": "Diseñado y Desarrollado por Sam Esteban con ",
+  },
+  en: {
+    "tab.title": "Sam Esteban | Full Stack Developer, Front-End oriented",
+    "nav.home": "Home",
+    "nav.portfolio": "Portfolio",
+    "nav.contact": "Contact",
+    "hero.hi": "Hi, I am",
+    "hero.name": "Sam Esteban",
+    "hero.job":
+      "Web Designer and Full Stack Developer, based on Front-End with +10 years in experience.",
+    "hero.years": "I have 35 rotations to the sun",
+    "hero.desk": "and my desktop is in Chile.",
+    "hero.knowledge": "My knowledge have:",
+    "btn.viewSite": "View Site",
+    "btn.viewPortfolio": "View Portfolio",
+    "title.portfolio": "Portfolio",
+    "title.contact": "Let's talk",
+    "footer.copyright": "Design and Developed by Sam Esteban with ",
+  },
+} as const;
