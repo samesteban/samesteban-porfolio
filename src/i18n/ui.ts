cc8b106a87883e0a0e3086cd5cf18db4965bbc1b
s.bustos@want.cl <s.bustos@want.cl>
2025-12-01 01:30:15 -0300

    feat: upadte CV and Portfolio

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -16,7 +16,7 @@ export const ui = {
     "hero.name": "Sam Esteban",
     "hero.job":
       "Diseñador web y Desarrollador Full Stack, orientado a Front-End con más de 14 años de experiencia.",
-    "hero.years": "Pronto completaré 37 vueltas al astro rey",
+    "hero.years": "Tengo 37 vueltas al astro rey",
     "hero.desk": "y mi escritorio actualmente está en Chile.",
     "hero.knowledge":
       "A través de mi viaje profesional, he aprendido una variedad de herramientas para traer ideas a la vida.",
@@ -35,7 +35,7 @@ export const ui = {
     "hero.name": "Sam Esteban",
     "hero.job":
       "a Web Designer and Full Stack Front-End oriented Developer with over 14 years of experience.",
-    "hero.years": "I will soon complete 37 trips around the sun,",
+    "hero.years": "I have 37 rounds of the astro rover",
     "hero.desk": "and my desktop station is currently in Chile.",
     "hero.knowledge":
       "Throughout my professional journey, I have curated a variety of tools that empower me to bring visions to life.",
